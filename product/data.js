cbc85d7c80822538b7b350768e0c5bd49e5e3279
Fuad Hasan Shihab <85599261+fuadhasanshihab@users.noreply.github.com>
2024-05-02 21:12:32 +0600

    Update data.js

--- product/data.js
+++ product/data.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
   const urlParams = new URLSearchParams(window.location.search);
   const paramValue = urlParams.get('p');
   const productsData = {
-    'demo1': {
+    'demo19': {
       name: 'Lorem Ipsum Bangla Typography Combo Pack',
       code: 'FHS9362',
       price: '800',
