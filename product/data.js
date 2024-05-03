25bfbcc496e6b6678c4f3275a75180aed7bb6f39
Fuad Hasan Shihab <85599261+fuadhasanshihab@users.noreply.github.com>
2024-05-03 12:25:10 +0600

    Update data.js

--- product/data.js
+++ product/data.js
@@ -1,11 +1,11 @@
 const productsData = {
   'demo1': {
-    name: 'Lorem Ipsum Bangla Typography Combo Pack',
+    name: 'Business Card PLP',
     code: 'FHS9362',
-    price: '800',
-    details: 'Description demo typo',
-    link: '/search/Label/Resources',
-    image: 'https://lh3.googleusercontent.com/B8Ry0wwh_7z_MqH4TwdJOnU2InT8AiX0G3tBf0H2WbgqvQygQvCjTyCrOmHmi213B66H4PdU4R1ZcrUFtLViPJ0gQsMm7-_lL9Sz8pGqC20bXkc9PgtOv_7AOuIenr4-uw=w260',
+    price: '50',
+    details: 'Quantity: 1',
+    link: 'https://fuadhasanshihab.blogspot.com/2023/12/business-card-plp.html',
+    image: 'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjA0yEsQ9ukjHaEe8Ur0Sgwb3jvZvdddMK039BpoCBb2dzH7PKnRXqiHX5gSF7l6alBPJFGmToNtRzNn99p76bqva9UgkKmiHxLpHP_iDmo_Nglg2MkoNnyIrCoCZWy8MumXHq1WqoU5qtACIhzbx4geVJZbpAtQiBytj0kVf8wq8IaiJr_5NBmiQrC56yQ/s1600/Business%20Card%20%5BFront%5D.jpg',
     form: 'https://docs.google.com/forms/d/e/1FAIpQLScWNZ2hGcTXJs7eF0Ysu5WuZOb904W6zN8Gvnqx6YIcgkazSg/viewform?embedded=true&usp=pp_url&entry.358930372=FHS3749&entry.723714736=DEMO+PRODUCT+NAME&entry.1724040677=500&entry.472063648=https://fuadhasanshihab.blogspot.com/2024/01/demo.html',
     formBank: 'https://docs.google.com/forms/d/e/1FAIpQLSf7NzwO8tSYj9k-lO7iKs3CKy4-m5nEudf5DVlEggk9d15HnQ/viewform?usp=pp_url&entry.358930372=FHS3749&entry.723714736=DEMO+PRODUCT+NAME&entry.1724040677=500&entry.472063648=https://fuadhasanshihab.blogspot.com/2024/01/demo.html'
   },
