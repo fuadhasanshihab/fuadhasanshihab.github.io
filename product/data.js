1d53d6f944c8b7c3ce02cd7c0c6b3b5f85f0d45f
Fuad Hasan Shihab <85599261+fuadhasanshihab@users.noreply.github.com>
2024-05-03 14:43:54 +0600

    Update data.js

--- product/data.js
+++ product/data.js
@@ -2,12 +2,12 @@ const productsData = {
   'FHS456': {
     name: 'Business Card PLP',
     code: 'FHS456',
-    previousPrice: '<b>100</b>',
-    discount: '50',
+    previousPrice: '৳100',
+    discount: '-৳50',
     price: '50', // main price
     previousPriceText: 'Subtotal',
     discountText: 'Discount (50%)',
-    details: 'Quantity: 1',
+    details: 'Digital Download',
     link: 'https://fuadhasanshihab.blogspot.com/2023/12/business-card-plp.html',
     image: 'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjA0yEsQ9ukjHaEe8Ur0Sgwb3jvZvdddMK039BpoCBb2dzH7PKnRXqiHX5gSF7l6alBPJFGmToNtRzNn99p76bqva9UgkKmiHxLpHP_iDmo_Nglg2MkoNnyIrCoCZWy8MumXHq1WqoU5qtACIhzbx4geVJZbpAtQiBytj0kVf8wq8IaiJr_5NBmiQrC56yQ/s1600/Business%20Card%20%5BFront%5D.jpg',
     form: 'https://docs.google.com/forms/d/e/1FAIpQLScWNZ2hGcTXJs7eF0Ysu5WuZOb904W6zN8Gvnqx6YIcgkazSg/viewform?embedded=true&usp=pp_url&entry.358930372=FHS3749&entry.723714736=DEMO+PRODUCT+NAME&entry.1724040677=500&entry.472063648=https://fuadhasanshihab.blogspot.com/2024/01/demo.html',
