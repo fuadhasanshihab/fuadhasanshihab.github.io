a5101dc3560e211730efa31b293d9d909e3a0065
Fuad Hasan Shihab <85599261+fuadhasanshihab@users.noreply.github.com>
2024-05-04 06:24:09 +0600

    Update data.js

--- product/data.js
+++ product/data.js
@@ -7,9 +7,9 @@ const productsData = {
     price: '50', // main price
     previousPriceText: 'Subtotal',
     discountText: 'Discount (50%)',
-    details: 'Digital Download',
+    details: 'Format: PLP',
     link: 'https://fuadhasanshihab.blogspot.com/2023/12/business-card-plp.html',
-    image: 'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjA0yEsQ9ukjHaEe8Ur0Sgwb3jvZvdddMK039BpoCBb2dzH7PKnRXqiHX5gSF7l6alBPJFGmToNtRzNn99p76bqva9UgkKmiHxLpHP_iDmo_Nglg2MkoNnyIrCoCZWy8MumXHq1WqoU5qtACIhzbx4geVJZbpAtQiBytj0kVf8wq8IaiJr_5NBmiQrC56yQ/s1600/Business%20Card%20%5BFront%5D.jpg',
+    image: 'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjR8K5ng_VUu0MGouzAPtBalZX1GbM2UQhb6m1Ed3Wd1jcvYG94AplXPHYyvU1gmsn-_Y2QziioOKB8OrGIdfVxy-40fkUTuGmA9enUSpo6xXgmJ6OS6lM40PcGm-Rs8dPWPfHRp7pNRB3b43njq8MrrOaWMP-Wrzfo6ideCyzDlLRF94oJZQZ1hdDC5HZq/s1600/business_card_thumb_fhs.png',
     form: 'https://docs.google.com/forms/d/e/1FAIpQLScWNZ2hGcTXJs7eF0Ysu5WuZOb904W6zN8Gvnqx6YIcgkazSg/viewform?embedded=true&usp=pp_url&entry.358930372=FHS3749&entry.723714736=DEMO+PRODUCT+NAME&entry.1724040677=500&entry.472063648=https://fuadhasanshihab.blogspot.com/2024/01/demo.html',
     formBank: 'https://docs.google.com/forms/d/e/1FAIpQLSf7NzwO8tSYj9k-lO7iKs3CKy4-m5nEudf5DVlEggk9d15HnQ/viewform?usp=pp_url&entry.358930372=FHS3749&entry.723714736=DEMO+PRODUCT+NAME&entry.1724040677=500&entry.472063648=https://fuadhasanshihab.blogspot.com/2024/01/demo.html'
   },
