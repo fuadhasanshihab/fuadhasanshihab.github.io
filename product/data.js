a85e3df1d39adf6f3dba32f7c333320fc3ff77d5
Fuad Hasan Shihab <85599261+fuadhasanshihab@users.noreply.github.com>
2024-05-04 06:52:47 +0600

    Update data.js

--- product/data.js
+++ product/data.js
@@ -3,7 +3,7 @@ const productsData = {
     name: 'Business Card PLP',
     details: 'Format: PLP',
     code: 'FHS456',
-    price: '50', // main price
+    price: '50', // current main p
     prePrice: '৳100',
     discount: '-৳50',
     prePriceText: 'Subtotal',
@@ -13,17 +13,18 @@ const productsData = {
     form: 'https://docs.google.com/forms/d/e/1FAIpQLScWNZ2hGcTXJs7eF0Ysu5WuZOb904W6zN8Gvnqx6YIcgkazSg/viewform?embedded=true&usp=pp_url&entry.358930372=FHS3749&entry.723714736=DEMO+PRODUCT+NAME&entry.1724040677=500&entry.472063648=https://fuadhasanshihab.blogspot.com/2024/01/demo.html',
     formBank: 'https://docs.google.com/forms/d/e/1FAIpQLSf7NzwO8tSYj9k-lO7iKs3CKy4-m5nEudf5DVlEggk9d15HnQ/viewform?usp=pp_url&entry.358930372=FHS3749&entry.723714736=DEMO+PRODUCT+NAME&entry.1724040677=500&entry.472063648=https://fuadhasanshihab.blogspot.com/2024/01/demo.html'
   },
-  'demo2': {
-    name: 'Dolor Sit Amet Bangla Font',
-    code: 'FHS9274',
-    previousPrice: '',
-    discountText: '',
+  'demo': {
+    name: 'Lorem Ipsum Bangla Typography Combo Pack',
+    details: 'Format: PNG, JPG, SVG',
+    code: 'FHS938',
+    price: '1000', // current main p
+    prePrice: '৳1000',
     discount: '',
-    price: '50',
-    details: 'detail aka',
-    link: '/product2.html',
+    prePriceText: 'Subtotal',
+    discountText: '',
+    link: 'https://fuadhasanshihab.blogspot.com/2023/12/demo.html',
     image: 'https://lh3.googleusercontent.com/B8Ry0wwh_7z_MqH4TwdJOnU2InT8AiX0G3tBf0H2WbgqvQygQvCjTyCrOmHmi213B66H4PdU4R1ZcrUFtLViPJ0gQsMm7-_lL9Sz8pGqC20bXkc9PgtOv_7AOuIenr4-uw=w260',
-    form: 'https://docs.google.com/forms/d/e/1FAIpQLScWNZ2hGcTXJs7eF0Ysu5WuZOb904W6zN8Gvnqx6YIcgkazSg/viewform?embedded=true&usp=pp_url&entry.358930372=FHS3749&entry.723714736=DEMO+PRODUCT+NAME&entry.1724040677=500+BDT&entry.472063648=https://fuadhasanshihab.blogspot.com/2024/01/demo.html',
+    form: 'https://docs.google.com/forms/d/e/1FAIpQLScWNZ2hGcTXJs7eF0Ysu5WuZOb904W6zN8Gvnqx6YIcgkazSg/viewform?embedded=true&usp=pp_url&entry.358930372=FHS3749&entry.723714736=DEMO+PRODUCT+NAME&entry.1724040677=500&entry.472063648=https://fuadhasanshihab.blogspot.com/2024/01/demo.html',
     formBank: 'https://docs.google.com/forms/d/e/1FAIpQLSf7NzwO8tSYj9k-lO7iKs3CKy4-m5nEudf5DVlEggk9d15HnQ/viewform?usp=pp_url&entry.358930372=FHS3749&entry.723714736=DEMO+PRODUCT+NAME&entry.1724040677=500&entry.472063648=https://fuadhasanshihab.blogspot.com/2024/01/demo.html'
   },
 };
